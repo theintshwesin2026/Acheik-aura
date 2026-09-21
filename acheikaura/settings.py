98bcb0c71043e35d40a95c58329a9c66957de812
theint <theintshwesin186200@email.com>
2026-09-22 00:47:17 +0630

    Configure Django for Vercel deployment

--- acheikaura/settings.py
+++ acheikaura/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'main/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ api/index.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acheikaura.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
